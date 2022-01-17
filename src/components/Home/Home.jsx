cbe1cbaf1ec1dcca2280e706e6b3f55b46a3a6d7
AntonioGSC <antoniogabrielagsc@gmail.com>
2022-01-17 12:49:33 -0300

    Finishing project

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import { GET_Products } from '../../APIs/APIs';
-import Item from '../Item/Item'
+import Item from '../Item/Item';
+
+import Logo from '../../assets/images/logo.png';
+import Email from '../../assets/images/mail-logo.png';
+import Phone from '../../assets/images/cellphone-logo.png';
+import AnaCarol from '../../assets/images/Ana-Carol.png';
 
 import './Home.css';
 
@@ -26,7 +31,20 @@ function Home() {
   return (
     <div className='home_div'>
       <header className="home_header">
-
+        <div className="home_header-container">
+          <div className="home_header-logo-div">
+            <img src={ Logo } alt="" className="home_header-logo" />
+          </div>
+          <div className="home_header-info-div">
+            <img src={ Email } alt="" className="home_header-function" />
+            <img src={ Phone } alt="" className="home_header-function" />
+            <img src={ AnaCarol } alt="" className="home_header-image-user" />
+            <div className="home_header-info-desc">
+              <p className="home_header-info home_header-name">Ana Carol Machado</p>
+              <p className="home_header-info home_header-date">Segunda, 17/01/2022</p>
+            </div>
+          </div>
+        </div>
       </header>
       <main className="home_main">
         {
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -12,6 +12,11 @@ function Item( {item } ) {
       <div className="item_main">
         <img src={item.imagem} alt="Imagem do Produto" className="item_main-image" />
         <p className="item_main-description">{item.descricao}</p>
+        <div className="item_main-price-div">
+          <p className="item_main-price-intro">a partir de</p>
+          <p className="item_main-price">R$ {parseFloat(item.preco).toFixed(2).replace('.',',')}</p>
+          <p className="item_main-price-footer">gerado pela melhor oferta</p>
+        </div>
       </div>
       <div className="item_footer">
         <button className='item_footer-button'>CONFIRA</button>
